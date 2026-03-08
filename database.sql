2c367a7993ce5579030a096da7df438a07ab7af2
bapatata688 <b0ldm6diego@gmail.com>
2026-03-08 17:30:56 -0600

    database.sql | created table products | schema:0.2 |

--- database.sql
+++ database.sql
@@ -1,3 +1,4 @@
 create database insal;
 use insal;
 create table user (id int primary key auto_increment, name varchar(50), price varchar(50), image varchar(50), description varchar(50));
+create table teacher (id int primary key auto_increment, name varchar(50), price varchar(50), image varchar(50), description varchar(50));
