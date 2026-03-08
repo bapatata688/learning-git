3a23b399d81f236cf90d7718772f883e414132d8
bapatata688 <b0ldm6diego@gmail.com>
2026-03-08 17:55:35 -0600

    database.sql | created docker file and compose | schema:0.4 |

--- database.sql
+++ database.sql
@@ -2,4 +2,8 @@ create database insal;
 use insal;
 create table user (id int primary key auto_increment, name varchar(50), price varchar(50), image varchar(50), description varchar(50));
 create table teacher (id int primary key auto_increment, name varchar(50), price varchar(50), image varchar(50), description varchar(50));
-insert into user ()
+insert into user (name, price, image, description) values ('samsung', 'price', 'image', 'description');
+insert into user (name, price, image, description) values ('samsung', 'price', 'image', 'description');
+insert into user (name, price, image, description) values ('samsung', 'price', 'image', 'description');
+insert into user (name, price, image, description) values ('samsung', 'price', 'image', 'description');
+insert into user (name, price, image, description) values ('samsung', 'price', 'image', 'description');
