43a3fac0ed61d760ae72ad47eac3c3563d074bbf
bapatata688 <b0ldm6diego@gmail.com>
2026-03-10 17:16:56 -0600

    sistema general |  datos de prueba |

--- database.sql
+++
@@ -1,4 +0,0 @@
-create database insal;
-use insal;
-create table user (id int primary key auto_increment, name varchar(50), price varchar(50), image varchar(50), description varchar(50));
-create table teacher (id int primary key auto_increment, name varchar(50), price varchar(50), image varchar(50), description varchar(50));
